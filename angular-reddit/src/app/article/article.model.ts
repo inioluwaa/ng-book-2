89eac613baf1f70395a06bb770c947e76d2fcd18
Inioluwa Akinleye <inioluwaakinleye@gmail.com>
2017-10-12 11:49:34 +0100

    render a list to the article

--- angular-reddit/src/app/article/article.model.ts
+++ angular-reddit/src/app/article/article.model.ts
@@ -16,6 +16,15 @@ export class Article {
   voteDown(): void {
     this.votes -= 1;
   }
+
+  domain(): string {
+    try {
+      const domainAndPath: string = this.link.split('//')[1];
+      return domainAndPath.split('/')[0];
+    } catch (err) {
+      return null;
+    }
+  }
 }
 
 
